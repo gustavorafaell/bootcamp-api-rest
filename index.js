0718bc3274aec278d0d5e93d3b2970f73775375d
gustavorafaell <gustavo_hbr@hotmail.com>
2022-11-09 20:46:29 -0300

    Adicionado rota CEP e aplicado ECMAScript

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express"); 
 const app = express(); //metodo de inicialização express
 const cliente = require("./routes/cliente"); 
-const pedido  = require("./routes/pedido")
+const pedido  = require("./routes/pedido");
+const cep = require("./routes/cep");
 /*permitir que a aplicação seja iniciada
 No arquivo JSON
 license...
@@ -13,8 +14,23 @@ yarn add -D nodemon*/
 app.use(express.json()); //poder ler o body da requisição
 app.use("/cliente", cliente);
 app.use("/pedido", pedido);
-app.listen(3000, function(){ //definir qual porta roda a aplicação 
+app.use("/cep", cep);
+
+app.listen(3000, () =>{ //definir qual porta roda a aplicação 
     console.log("Aplicação rodando na porta 3000");
+
+    /*
+    ECMAScript
+    const produto = {
+      nome: "Arroz",
+      preco: 20,
+    };
+    console.log(produto);
+    console.log(produto.nome);
+
+    const {nome} = produto;
+    console.log(nome);*/
+
 });
 
 
