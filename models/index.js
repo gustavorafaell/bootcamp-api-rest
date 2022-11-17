8715ec4961d5c6f787cb22a7153020983202f149
gustavorafaell <gustavo_hbr@hotmail.com>
2022-11-16 22:39:40 -0300

    Criado models

---
+++ models/index.js
@@ -0,0 +1,20 @@
+const { Sequelize } = require("sequelize");
+const { initModels } = require("./init-models");
+
+const options = {
+  username: "postgres",
+  password: "postgres",
+  database: "ecommerce",
+  host: "localhost",
+  dialect: "postgres",
+};
+
+const connection = new Sequelize(options);
+connection
+  .authenticate()
+  .then(() => console.log("Conectado ao banco de dados"))
+  .catch((erro) => console.log("Erro ao conectar ao banco de dados", erro));
+
+let db = initModels(connection);
+db = { ...db, connection };
+module.exports = db;
