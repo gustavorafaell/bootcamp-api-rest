db527205501db85a8a74d3d4f7d372e69fb84df1
gustavorafaell <gustavo_hbr@hotmail.com>
2022-11-16 22:40:23 -0300

    atualizado rota cliente

--- routes/cliente.js
+++ routes/cliente.js
@@ -1,17 +1,22 @@
 const { Router} = require("express"); // pega somente o recurso Router do express
+const connection = require("../models")
+const { criar, atualizar, remover } = require("../controllers/cliente");
 const router = Router();
 
 router.get("/:id?", (req, res) =>{ //? para deixar o parametro id opcional
     res.send("Listar cliente");
 });
-router.post("/", (req, res) =>{ //ARROW Functions
-    res.send("Criar cliente");
+router.post("/", async(req, res) =>{ //ARROW Functions
+    const result = await criar(req.body);
+    res.send(result);
 });
-router.put("/:id", (req, res) => { // parametro id obrigatório
-    res.send("Atualizar cliente");
+router.put("/:id", async(req, res) => { // parametro id obrigatório
+    const result = await atualizar(req.params.id, req.body);
+    res.send(result);
 });
-router.delete("/:id", (req, res) =>{ //ARROW Functions define uma funcao
-    res.send("Excluir cliente");
+router.delete("/:id", async(req, res) =>{ //ARROW Functions define uma funcao
+    const result = await remover(req.params.id);
+    res.send();
 })
 
 module.exports = router;
