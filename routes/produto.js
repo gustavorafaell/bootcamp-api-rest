7f8256981a56abb96cf2c6dcbd440340d1e92a07
gustavorafaell <gustavo_hbr@hotmail.com>
2022-11-23 20:51:29 -0300

    adicionado rota de produto

---
+++ routes/produto.js
@@ -0,0 +1,49 @@
+const { Router } = require("express");
+const router = Router();
+const { buscarTodos, buscarPorId } = require("../controllers/basic");
+const { criar, atualizar, remover, buscar } = require("../controllers/produto");
+const { produto } = require("../models");
+
+router.get("/:id?", async (req, res) => {
+  try {
+    const result = req.params.id
+      ? await buscarPorId(produto, req.params.id)
+      : await buscarTodos(produto);
+  
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({mensagem: error.message});
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const result = await criar(req.body);
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({mensagem: error.message});
+  }
+});
+
+router.put("/:id", async (req, res) => {
+  try {
+    await atualizar(req.params.id, req.body);
+    const result = await buscar(req.params.id);
+  
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({mensagem: error.message});
+  }
+});
+
+router.delete("/:id", async (req, res) => {
+  try {
+    await remover(req.params.id);
+  
+    res.send();
+  } catch (error) {
+    res.status(500).send({mensagem: error.message});
+  }
+});
+
+module.exports = router;
